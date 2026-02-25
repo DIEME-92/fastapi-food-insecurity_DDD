cd3170c0fa57fe85e0753aea1206a9d5d42bfae2
DIEME <89424921+DIEME-92@users.noreply.github.com>
2026-02-25 13:50:58 +0000

    Refactor app.py for improved readability and structure

--- app.py
+++ app.py
@@ -14,9 +14,11 @@ app = FastAPI()
 
 # Variables utilisées pour la prédiction individuelle
 selected_features = [
-    "q604_manger_moins_que_ce_que_vous_auriez_du",
+     "q606_1_avoir_faim_mais_ne_pas_manger",
     "q605_1_ne_plus_avoir_de_nourriture_pas_suffisamment_d_argent",
-    "q606_1_avoir_faim_mais_ne_pas_manger"
+    "q604_manger_moins_que_ce_que_vous_auriez_du",
+    "q603_sauter_un_repas",
+    "q601_ne_pas_manger_nourriture_saine_nutritive"
 ]
 
 # Schéma d'entrée pour prédiction individuelle
@@ -72,5 +74,6 @@ def predict(data: InputData):
             "error": "Une erreur est survenue",
             "details": str(e)
         }, status_code=500)
+
 
 
